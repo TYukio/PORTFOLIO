a24e333e0d48f21a790a8e095917fb9cb4cd6909
tiago.izumi <tiago.y.izumi@exxonmobil.com>
2023-03-03 11:36:22 -0300

    changing the cards and the button

--- src/Components/cards.js
+++ src/Components/cards.js
@@ -4,16 +4,16 @@ function Cards(){
         <main>
           <div class="cards">
         <div class="card1">
-        <img src="react.png"></img>
+        <img src="logo192.png"></img>
             <h1>React</h1>  
         </div>
         <div class="card2">
         <img src="react.png"></img>
-            <h1>React</h1>  
+            <h1></h1>  
         </div>
         <div class="card3">
-        <img src="react.png"></img>
-            <h1>React</h1>  
+        <img src="css.png"></img>
+            <h1>CSS</h1>  
         </div>
 
     </div>
